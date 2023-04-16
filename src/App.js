1185b89c0342490aae26c8125c560f70cc7173e9
Umut Yenidil <umut.yenidil9@gmail.com>
2023-04-17 02:54:25 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, {useEffect} from "react";
 import './App.css';
+import LoginForm from "./components/LoginForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    useEffect(() => {
+        const link = document.createElement("link");
+        link.href = "https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css";
+        link.rel = 'stylesheet';
+        document.head.appendChild(link);
+    }, []);
+
+    return (
+        <div className='app'>
+            <LoginForm/>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/LoginForm.js
@@ -0,0 +1,71 @@
+import React, {useState} from "react";
+import './LoginForm.css';
+
+const LoginForm = () => {
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredPassword, setEnteredPassword] = useState('');
+    const usernameChangeHandler = (event) => {
+        let username = event.target.value;
+        setEnteredUsername(username);
+    };
+    const passwordChangeHandler = (event) => {
+        let password = event.target.value;
+        setEnteredPassword(password);
+    };
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+
+        const formData = {
+            username: enteredUsername,
+            password: enteredPassword,
+        };
+
+        console.log(formData);
+
+        setEnteredUsername('');
+        setEnteredPassword('');
+    };
+
+    return (
+        <div className='container'>
+            <div className='wrapper'>
+                <div className='title'>
+                    <span>Login</span>
+                </div>
+                <form onSubmit={formSubmitHandler}>
+                    <div className='row'>
+                        <i className='bx bxs-user'></i>
+                        <input type='text'
+                               placeholder='Email or Username'
+                               onChange={usernameChangeHandler}
+                               value={enteredUsername}/>
+                    </div>
+                    <div className='row'>
+                        <i className='bx bxs-lock-alt'></i>
+                        <input type='password'
+                               placeholder='Password'
+                               onChange={passwordChangeHandler}
+                               value={enteredPassword}/>
+                    </div>
+                    <div className='forgot-password-link'>
+                        <a href='test.com'>
+                            Forgot Password?
+                        </a>
+                    </div>
+                    <div className='row button'>
+                        <input type='submit' value='Login'/>
+                    </div>
+                    <div className='signup-link'>
+                        <span>Not a member? </span>
+                        <a href='test.com'>
+                            Signup now
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default LoginForm;
