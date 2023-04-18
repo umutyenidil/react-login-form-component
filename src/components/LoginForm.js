7fe3d00b33673309faf9ca64c2dc6b5e6bf5cdb9
Umut Yenidil <umut.yenidil9@gmail.com>
2023-04-19 00:38:45 +0300

    style'lar css module'ler ile degistirildi

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import './LoginForm.css';
+import styles from './LoginForm.module.css';
 
 const LoginForm = () => {
     const [enteredUsername, setEnteredUsername] = useState('');
@@ -28,35 +28,35 @@ const LoginForm = () => {
     };
 
     return (
-        <div className='container'>
-            <div className='wrapper'>
-                <div className='title'>
+        <div className={`${styles['container']}`}>
+            <div className={`${styles['wrapper']}`}>
+                <div className={`${styles['title']}`}>
                     <span>Login</span>
                 </div>
                 <form onSubmit={formSubmitHandler}>
-                    <div className='row'>
+                    <div className={`${styles['row']}`}>
                         <i className='bx bxs-user'></i>
                         <input type='text'
                                placeholder='Email or Username'
                                onChange={usernameChangeHandler}
                                value={enteredUsername}/>
                     </div>
-                    <div className='row'>
+                    <div className={`${styles['row']}`}>
                         <i className='bx bxs-lock-alt'></i>
                         <input type='password'
                                placeholder='Password'
                                onChange={passwordChangeHandler}
                                value={enteredPassword}/>
                     </div>
-                    <div className='forgot-password-link'>
+                    <div className={`${styles['forgot-password-link']}`}>
                         <a href='test.com'>
                             Forgot Password?
                         </a>
                     </div>
-                    <div className='row button'>
+                    <div className={`${styles['row']} ${styles['button']}`}>
                         <input type='submit' value='Login'/>
                     </div>
-                    <div className='signup-link'>
+                    <div className={`${styles['signup-link']}`}>
                         <span>Not a member? </span>
                         <a href='test.com'>
                             Signup now
